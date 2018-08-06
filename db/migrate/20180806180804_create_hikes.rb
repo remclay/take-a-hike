6c79eec834f5d1bbba142ba44053072ae3112fd6
remclay <remmy.lauren@gmail.com>
2018-08-06 12:31:47 -0600

    Add seed data, change hike rating type to float.

--- db/migrate/20180806180804_create_hikes.rb
+++ db/migrate/20180806180804_create_hikes.rb
@@ -4,7 +4,7 @@ class CreateHikes < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.string :difficulty
-      t.integer :rating
+      t.float :rating
       t.integer :hike_id
 
       t.timestamps
