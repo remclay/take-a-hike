996a93a76560cfd9e728f5883fb8741134b662cd
remclay <remmy.lauren@gmail.com>
2018-08-06 14:18:56 -0600

    Add Client.js file to fetch from api, add HikeSearch (temporarily).

---
+++ client/src/HikeSearch.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Client from './Client';
+
+class HikeSearch extends React.Component {
+  state = {
+    hikes: [],
+    searchValue: '',
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value;
+
+    this.setState({
+      searchValue: value,
+    });
+
+  Client.search(value, (hikes) => {
+    this.setState({
+      hikes: hikes,
+    });
+  })
+}
+
+  render() {
+    const hikeRows = this.state.hikes.map((hike, id) => (
+      <li key={id}>
+        {hike.name} - {hike.location}
+      </li>
+    ));
+
+    return (
+      <div id='hike-search'>
+        <ul>
+          {hikeRows}
+        </ul>
+            <input
+              className='prompt'
+              type='text'
+              placeholder='Search hikes...'
+              value={this.state.searchValue}
+              onChange={this.handleChange}
+            />
+      </div>
+    );
+  }
+}
+
+export default HikeSearch;
