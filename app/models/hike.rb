59e7109d6bcc9cce12a2e3a9a6b80ea27fb39dc6
remclay <remmy.lauren@gmail.com>
2018-08-07 10:17:13 -0600

    Add ActiveRecord associations to models.

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,4 +1,6 @@
 class Hike < ApplicationRecord
+  has_many :comments
+  has_many :users, through: :comments
   validates :name, presence: true
   validates :location, presence: true
   validates :difficulty, presence: true
