e49e66befedb7637d31ddef16af35f511835a87d
remclay <remmy.lauren@gmail.com>
2018-08-07 10:30:16 -0600

    Add has_secure_password and bcrypt to user model and gemfile. Add validations to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews
   has_many :hikes, through: :reviews
+
+  validates :name, :email, presence: true
+  validates :password, length: { in: 6..20 }
 end
