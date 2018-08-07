9b5588103898abdb505f4ea55c3a72d0e62a191c
remclay <remmy.lauren@gmail.com>
2018-08-07 10:35:49 -0600

    Add validations to hike model and add further validations to user model.

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,7 +1,7 @@
 class Hike < ApplicationRecord
   has_many :reviews
   has_many :users, through: :reviews
-  validates :name, presence: true
-  validates :location, presence: true
-  validates :difficulty, presence: true
+
+  validates :name, :location, :difficulty, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
 
   validates :name, :email, presence: true
   validates :password, length: { in: 6..20 }
+  validates :email, uniqueness: true
+  validates :email, :email_format => { :message => "email address must be valid"}
 end
