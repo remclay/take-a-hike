8acb53f3c7418294ae6256de8808b8bf4cc2c943
remclay <remmy.lauren@gmail.com>
2018-08-07 11:55:39 -0600

    Stub out basic actions in hikes_controller.

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,5 +1,33 @@
 class HikesController < ApplicationController
+  before_action :set_hike, only: :show
+
+  # GET /hikes
   def index
     @hikes = Hike.all
+    render json: @hikes
   end
+
+  # GET /hikes/1
+  def show
+    render json: @hike
+  end
+
+  # POST /hikes
+  def create
+    @hike = Hike.new(hike_params)
+    if @hike.save
+      render json: @hike
+    else
+      render json: @hike.errors
+    end
+  end
+
+  private
+    def set_hike
+      @hike = Hike.find(params[:id])
+    end
+
+    def hike_params
+      params.require(:hike).permit(:name, :location, :difficulty)
+    end
 end
