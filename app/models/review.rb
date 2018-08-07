e6f08ed7b36c3181008793b4f32f0ca63e6aaf31
remclay <remmy.lauren@gmail.com>
2018-08-07 11:04:04 -0600

    Add validations to review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :hike
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: true
+  validates_inclusion_of :rating, in: 1.0..5.0
 end
