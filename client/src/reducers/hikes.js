86a44d10390c52e0f331d2bb0dc60e4ef48f59cd
remclay <remmy.lauren@gmail.com>
2018-08-08 15:10:01 -0600

    Fix typos in hikes reducer.

--- client/src/reducers/hikes.js
+++ client/src/reducers/hikes.js
@@ -9,8 +9,8 @@ function hikesReducer(state = {loading: false, hikes: []}, action) {
       return {loading: false, hikes: [...state.hikes, hike]}
     case 'UPDATE_HIKE':
       const hikeDetails = action.payload;
-      const toUpdate = state.hikes.find(el => el.id === updatedHike.id)
-      const index = state.hikes.indexOf(recipeToUpdate)
+      const toUpdate = state.hikes.find(el => el.id === hikeDetails.id)
+      const index = state.hikes.indexOf(toUpdate)
       return {loading: false, hikes: [...state.hikes.slice(0, index), hikeDetails, ...state.hikes.slice(index + 1)]}
     case 'DELETE_HIKE':
       // pass id to action
