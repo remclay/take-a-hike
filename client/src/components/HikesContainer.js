7d0ec478644340270d0e74a5305117e4a1b09742
remclay <remmy.lauren@gmail.com>
2018-08-08 15:13:30 -0600

    Amend HikesContainer, render both HikeForm and Hikes, fetch from API upon mounting.

--- client/src/components/HikesContainer.js
+++ client/src/components/HikesContainer.js
@@ -2,32 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions/hikeActions';
+import Hikes from './Hikes';
+import HikeForm from './HikeForm';
+
 
 class HikesContainer extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hikes: []
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     hikes: []
+  //   }
+  // }
 
 componentDidMount() {
   console.log("HC mounted!")
   this.props.actions.fetchHikes()
 }
 
+onHikeClick(id) {
+  // console.log("On hike clicky")
+}
+
   render() {
     return (
       <div className="hikes-container">
-          <h1>Hikes:</h1>
-          <p>{this.props.hikes.map(hike => <p>{hike.name}</p>)}</p>
+        <h1>Hikes</h1>
+        < Hikes
+          hikes={this.props.hikes}
+          onHikeClick={this.onHikeClick}
+        />
+        <h1>Add a new Hike</h1>
+        < HikeForm />
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
-  console.log(state.hikes.hikes)
+  // console.log(state.hikes.hikes)
   return {
     hikes: state.hikes.hikes
   }
