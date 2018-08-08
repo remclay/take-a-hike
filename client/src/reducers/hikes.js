83882a0cab4aa82e627808fdf0529faa8c869f26
remclay <remmy.lauren@gmail.com>
2018-08-08 12:34:13 -0600

    Stub out CRUD cases in reducer.

--- client/src/reducers/hikes.js
+++ client/src/reducers/hikes.js
@@ -4,15 +4,18 @@ function hikesReducer(state = {loading: false, hikes: []}, action) {
       return {...state, loading: true}
     case 'FETCH_HIKES':
       return {...state, hikes: action.payload};
-    // case 'ADD_HIKE':
-    //   return [
-    //     ...state,
-    //     {
-    //       id: action.id,
-    //       location: action.location,
-    //       difficulty: action.difficulty
-    //     }
-    //   ]
+    case 'ADD_HIKE':
+      const hike = action.payload
+      return {loading: false, hikes: [...state.hikes, hike]}
+    case 'UPDATE_HIKE':
+      const hikeDetails = action.payload;
+      const toUpdate = state.hikes.find(el => el.id === updatedHike.id)
+      const index = state.hikes.indexOf(recipeToUpdate)
+      return {loading: false, hikes: [...state.hikes.slice(0, index), hikeDetails, ...state.hikes.slice(index + 1)]}
+    case 'DELETE_HIKE':
+      // pass id to action
+      const newHikes = state.hikes.filter(el => el.id !== action.payload)
+      return {loading: false, hikes: newHikes}
     default:
       return state
   }
