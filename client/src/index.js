2e1a1a7f459582459383e3bad289aa1485c31e63
remclay <remmy.lauren@gmail.com>
2018-08-09 11:03:30 -0600

    Clean up import statements and amend render accordingly.

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom'
-// import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { store } from './store'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
