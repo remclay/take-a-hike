8088516ec5644df947a31882950d1cbed1372202
remclay <remmy.lauren@gmail.com>
2018-08-09 11:11:46 -0600

    Add router to App component, stub out routes for hike create, update, show and index and home.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
 import HikesContainer from './components/HikesContainer'
+import HikeForm from './components/HikeForm'
+import HikeUpdateForm from './components/HikeUpdateForm'
+import Hike from './components/Hike'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Take-a-hike</h1>
-        </header>
-        < HikesContainer />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Take-a-hike</h1>
+          </header>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/hikes' component={HikesContainer} />
+          <Switch>
+            <Route path='/hikes/new' component={HikeForm} />
+            <Route path='/hikes/:id/edit' component={HikeUpdateForm} />
+            <Route path='/hikes/:id' component={Hike} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
