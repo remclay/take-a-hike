34a9a7a65887fcc3d267eff471e7da9928f2413c
remclay <remmy.lauren@gmail.com>
2018-08-09 11:13:06 -0600

    Create Home component and begin to code links to other pages, rendering of search bar.

---
+++ client/src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+import HikeSearch from './HikeSearch';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div className="home-container">
+        <h1>Welcome to Take-a-hike</h1>
+        <p><Link to={`/hikes`}>Browse all hikes</Link></p>
+        <p>Search: < HikeSearch /></p>
+        <p>Browse by location: </p>
+        <p>Can't find what you're looking for? <Link to={`/hikes/new`}>Add a new hike</Link></p>
+      </div>
+    );
+  }
+}
+
+export default Home
