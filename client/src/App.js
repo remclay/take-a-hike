c2917a122db636c70d767bae99fe4ba68906ce46
remclay <remmy.lauren@gmail.com>
2018-08-09 17:51:21 -0600

    Add HikeShow component to '/hikes/:id' route. Render NavBar component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home'
+import NavBar from './components/NavBar'
 import HikesContainer from './components/HikesContainer'
 import HikeForm from './components/HikeForm'
 import HikeUpdateForm from './components/HikeUpdateForm'
-import Hike from './components/Hike'
+import HikeShow from './components/HikeShow'
+import Home from './components/Home'
 
 class App extends Component {
   render() {
@@ -15,12 +16,13 @@ class App extends Component {
           <header className="App-header">
             <h1 className="App-title">Take-a-hike</h1>
           </header>
+          <NavBar />
           <Route exact path='/' component={Home} />
           <Route exact path='/hikes' component={HikesContainer} />
           <Switch>
             <Route path='/hikes/new' component={HikeForm} />
             <Route path='/hikes/:id/edit' component={HikeUpdateForm} />
-            <Route path='/hikes/:id' component={Hike} />
+            <Route path='/hikes/:id' component={HikeShow} />
           </Switch>
         </div>
       </Router>
