d6453b23747ff5210ce0aff35603e2cc4369d1ee
remclay <remmy.lauren@gmail.com>
2018-08-10 14:49:09 -0600

    Add destroy action to HikesController, add comments to each action.

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,5 +1,5 @@
 class HikesController < ApplicationController
-  before_action :set_hike, only: [:show, :update]
+  before_action :set_hike, only: [:show, :update, :destroy]
 
   # GET /hikes
   def index
@@ -7,7 +7,7 @@ class HikesController < ApplicationController
     render json: @hikes
   end
 
-  # GET /hikes/1
+  # GET /hikes/:id
   def show
     render json: @hike
   end
@@ -22,12 +22,22 @@ class HikesController < ApplicationController
     end
   end
 
+  # PUT/PATCH /hikes/:id/edit
   def update
     @hike.update_attributes(hike_params)
     if @hike.save
       render json: @hike
     else
       render json: @hike.errors
+    end
+  end
+
+  # DELETE /hikes/:id
+  def destroy
+    if @hike.destroy
+      head :no_content, status: :ok
+    else
+      render json: @hike.errors, status: :unprocessable_entity
     end
   end
 
