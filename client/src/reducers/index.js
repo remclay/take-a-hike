22f59b34120cba3302e62d5fd20ccc989657c2e4
remclay <remmy.lauren@gmail.com>
2018-08-10 16:41:31 -0600

    Work on visibilityFilter reducer and add to combineReducers in index.js.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
 import hikes from './hikes'
-// import visibilityFilter from './visibilityFilter'
+import visibilityFilter from './visibilityFilter'
 
 export default combineReducers({
-    hikes
+    hikes,
+    visibilityFilter
 })
