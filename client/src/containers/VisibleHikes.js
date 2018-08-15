723f0cc8e79c89283ed51bc14b6a6d4709d1716c
remclay <remmy.lauren@gmail.com>
2018-08-15 16:11:00 -0600

    Amend VisibleHikes container to include filtering based on query (search term), refactor into if/else statements.

--- client/src/containers/VisibleHikes.js
+++ client/src/containers/VisibleHikes.js
@@ -1,25 +1,28 @@
 import { connect } from 'react-redux'
 import Hikes from '../components/Hikes'
-import { VisibilityFilters } from '../actions/hikeActions'
+// import { VisibilityFilters } from '../actions/hikeActions'
 
-const getVisibleHikes = (hikes, filter) => {
+const getVisibleHikes = (hikes, filter, query) => {
   // debugger
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return hikes.hikes
-    case VisibilityFilters.SHOW_EASY:
-      return hikes.hikes.filter(h => h.difficulty === "Easy")
-    case VisibilityFilters.SHOW_MODERATE:
-      return hikes.hikes.filter(h => h.difficulty === "Moderate")
-    case VisibilityFilters.SHOW_HARD:
-      return hikes.hikes.filter(h => h.difficulty === "Hard")
-    default:
-      throw new Error('Unknown filter: ' + filter)
+  let matchedHikes = hikes.hikes
+  if (query !== '') {
+    matchedHikes = matchedHikes.filter((el) => (el.name.toLowerCase().includes(query.toLowerCase())))
+  }
+  if (filter === "SHOW_ALL") {
+    return matchedHikes
+  } else if (filter === "SHOW_EASY") {
+    return matchedHikes.filter(h => h.difficulty === "Easy")
+  } else if (filter === "SHOW_MODERATE") {
+    return matchedHikes.filter(h => h.difficulty === "Moderate")
+  } else if (filter === "SHOW_HARD") {
+    return matchedHikes.filter(h => h.difficulty === "Hard")
+  } else {
+    throw new Error('Unknown filter: ' + filter)
   }
 }
 
 const mapStateToProps = state => ({
-  hikes: getVisibleHikes(state.hikes, state.visibilityFilter)
+  hikes: getVisibleHikes(state.hikes, state.visibilityFilter, state.queryFilter)
 })
 
 export default connect(mapStateToProps)(Hikes)
