795d8e9a326160343371d4eeac376208d90ad57f
remclay <remmy.lauren@gmail.com>
2018-08-15 16:43:46 -0600

    Move NavBar stlying into css file utilizing active and inactive className property. Adjust component padding and margin to have conformity when search bar is/isn't displayed.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-// later move to css file
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'white',
-  textDecoration: 'none',
-  color: 'black',
-}
-
-const activeLink = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'white',
-  textDecoration: 'underline',
-  color: 'black',
-}
-
 const NavBar = () => (
   <div className="navigation">
-    <NavLink to="/" exact style={link} activeStyle={activeLink}>Home</NavLink>
-    <NavLink to="/hikes" exact style={link} activeStyle={activeLink}>All hikes</NavLink>
-    <NavLink to="/hikes/new" exact style={link} activeStyle={activeLink}>New</NavLink>
+    <NavLink to="/" exact className="nav-link" activeClassName="active-nav-link">Home</NavLink>
+    <NavLink to="/hikes" exact className="nav-link" activeClassName="active-nav-link">All hikes</NavLink>
+    <NavLink to="/hikes/new" exact className="nav-link" activeClassName="active-nav-link">New</NavLink>
   </div>
 )
 
