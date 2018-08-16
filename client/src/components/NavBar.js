df3b2ada686a8452cb0373a8e20c7a37bd9be8c1
remclay <remmy.lauren@gmail.com>
2018-08-15 21:24:13 -0600

    Amend NavBar to reset query and visibility filter when 'All hikes' link is clicked, using mapDispatchToProps.

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { setVisibilityFilter, setQuery } from '../actions/hikeActions'
 
-const NavBar = () => (
+const NavBar = ({ onClick }) => (
   <div className="navigation">
     <NavLink to="/" exact className="nav-link" activeClassName="active-nav-link">Home</NavLink>
-    <NavLink to="/hikes" exact className="nav-link" activeClassName="active-nav-link">All hikes</NavLink>
+    <NavLink to="/hikes" exact className="nav-link" activeClassName="active-nav-link" onClick={onClick}>All hikes</NavLink>
     <NavLink to="/hikes/new" exact className="nav-link" activeClassName="active-nav-link">New</NavLink>
   </div>
 )
 
-export default NavBar;
+const mapDispatchToProps = (dispatch) => ({
+  onClick: () => {
+    dispatch(setVisibilityFilter("SHOW_ALL")),
+    dispatch(setQuery(''))
+  }
+})
+
+
+export default connect(null, mapDispatchToProps)(NavBar)
