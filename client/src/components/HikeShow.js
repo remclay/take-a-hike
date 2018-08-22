fe55975adb8a928cc00a175fe25e5635c10c311a
remclay <remmy.lauren@gmail.com>
2018-08-22 10:43:55 -0600

    Refactor HikeShow component into class, map state.hikes state to props and add conditional fetching of all hikes if state.hikes is empty (if navigate directly to this page or refresh, as state isn't persisted).

--- client/src/components/HikeShow.js
+++ client/src/components/HikeShow.js
@@ -2,42 +2,63 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Hike from './Hike';
-import { deleteHike } from '../actions/hikeActions';
+import { bindActionCreators } from 'redux';
+// import { deleteHike } from '../actions/hikeActions';
+import * as actions from '../actions/hikeActions';
 
-const HikeShow = (props) => {
+class HikeShow extends React.Component {
 
-  const handleDelete = () => {
-    props.deleteHike(props.hike);
-    props.history.push('/hikes')
+  componentDidMount() {
+    if (!this.props.hikes.length) {
+      console.log("I'm fetching Hikes")
+      this.props.actions.fetchHikes()
+    }
   }
 
-  return (
-    <div className="hikes-show-container">
-      { props.hike.id &&
-      <div>
-        < Hike
-          key={props.hike.id}
-          {...props.hike}
-          display="single"
-        />
-        <Link to={`/hikes/${props.hike.id}/edit`} className="editButton">Edit</Link>
-        <span className="deleteButton" onClick={() => { if (window.confirm("Are you sure you want to delete this hike?")) handleDelete() }}>Delete</span>
+  handleDelete = () => {
+    this.props.actions.deleteHike(this.props.hike);
+    this.props.history.push('/hikes')
+  }
+
+  render() {
+    return (
+      <div className="hikes-show-container">
+        { this.props.hike.id &&
+        <div>
+          < Hike
+            key={this.props.hike.id}
+            {...this.props.hike}
+            display="single"
+          />
+          <Link to={`/hikes/${this.props.hike.id}/edit`} className="editButton">Edit</Link>
+          <span className="deleteButton" onClick={() => { if (window.confirm("Are you sure you want to delete this hike?")) this.handleDelete() }}>Delete</span>
+        </div>
+        }
+        { !this.props.hike.id &&
+          <h3>Oops, we couldn&apos;t find that hike.</h3>
+        }
       </div>
-      }
-      { !props.hike.id &&
-        <h3>Oops, we couldn&apos;t find that hike.</h3>
-      }
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
   const selectedHike = state.hikes.hikes.find(h => h.id === parseInt(ownProps.match.params.id, 10));
   if (selectedHike) {
-    return { hike: selectedHike}
+    return {
+      hikes: state.hikes.hikes,
+      hike: selectedHike
+    }
   } else {
-    return { hike: {} }
+    return {
+      hikes: state.hikes.hikes,
+      hike: {}
+    }
   }
 }
 
-export default connect(mapStateToProps, { deleteHike })(HikeShow);
+const mapDispatchToProps = (dispatch) => {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HikeShow);
