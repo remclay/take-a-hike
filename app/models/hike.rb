ed98ff4549da61836da7b96da1af533b9a52cac8
remclay <remmy.lauren@gmail.com>
2018-08-21 21:49:28 -0600

    Amend hike model to remove Active Record Associations.

--- app/models/hike.rb
+++ app/models/hike.rb
@@ -1,7 +1,4 @@
 class Hike < ApplicationRecord
-  has_many :reviews
-  has_many :users, through: :reviews
-
   validates :name, :location, :difficulty, :description, presence: true
   validates :name, uniqueness: true
 end
