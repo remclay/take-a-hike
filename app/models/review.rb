8e1744c510d84268585b75fa2094de5412014462
remclay <remmy.lauren@gmail.com>
2018-08-21 21:48:39 -0600

    Delete unsures models - user and review.

--- app/models/review.rb
+++
@@ -1,8 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :user
-  belongs_to :hike
-
-  validates :content, :rating, presence: true
-  validates :rating, numericality: true
-  validates_inclusion_of :rating, in: 1.0..5.0
-end
