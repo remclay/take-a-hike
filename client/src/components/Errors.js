d643d0a97e99be0e889bb90ef1735ca949b9a620
remclay <remmy.lauren@gmail.com>
2018-08-25 14:53:22 -0600

    Begin refactoring Errors component to render render Error component for each message (work in progress).

--- client/src/components/Errors.js
+++ client/src/components/Errors.js
@@ -1,17 +1,24 @@
 import React from 'react'
 
+const Error = ({ prop, message }) => {
+
+  return (
+    <li>
+    {prop}: {message}
+    </li>
+  );
+}
+
 const Errors = ({ messages }) => {
-  let sentence = `<ul>`
-  for(const prop in messages) {
-      sentence += `<li>${prop} ${messages[prop]}</li>`
+  const errors = for (const prop in messages) {
+    <Error prop={prop} message={messages[prop]} />
   }
-  sentence += `</ul>`
 
   return (
     <div className="error-container">
       <p>The hike was not added, due to the following errors:</p>
       <div className="errors">
-        {sentence}
+      {errors}
       </div>
     </div>
   );
