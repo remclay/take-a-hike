ba8f95465dd8c1f2e7104f82acfb72810fb82063
remclay <remmy.lauren@gmail.com>
2018-08-27 09:49:10 -0600

    Add clearing of error array into all hikesReducer cases, amend default state from empty string to empty array.

--- client/src/reducers/hikes.js
+++ client/src/reducers/hikes.js
@@ -1,28 +1,27 @@
-function hikesReducer(state = {loading: false, hikes: [], errors: ""}, action) {
+function hikesReducer(state = {loading: false, hikes: [], errors: []}, action) {
   switch (action.type) {
     case 'LOADING_HKES':
-      return {...state, loading: true}
+      return {...state, errors: [], loading: true}
     case 'FETCH_HIKES':
-      return {...state, hikes: action.payload};
+      return {...state, errors: [], hikes: action.payload};
     // case 'SEARCH_HIKES':
     //   const term = action.payload
     //   const filteredHikes = state.hikes.filter((el) => (el.name.toLowerCase().includes(term.toLowerCase())))
     //   return {loading: false, hikes: filteredHikes}
     case 'ADD_HIKE_SUCCESS':
       const hike = action.payload
-      return {loading: false, hikes: [...state.hikes, hike], errors: []}
-      debugger
+      return {loading: false, errors: [], hikes: [...state.hikes, hike]}
     case 'ADD_HIKE_FAILURE':
       return {...state, errors: [...state.errors, action.message]}
     case 'UPDATE_HIKE':
       const hikeDetails = action.payload;
       const toUpdate = state.hikes.find(el => el.id === hikeDetails.id)
       const index = state.hikes.indexOf(toUpdate)
-      return {loading: false, hikes: [...state.hikes.slice(0, index), hikeDetails, ...state.hikes.slice(index + 1)]}
+      return {loading: false, errors: [], hikes: [...state.hikes.slice(0, index), hikeDetails, ...state.hikes.slice(index + 1)]}
     case 'DELETE_HIKE':
       // pass id to action
       const newHikes = state.hikes.filter(el => el.id !== action.payload)
-      return {loading: false, hikes: newHikes}
+      return {loading: false, errors: [], hikes: newHikes}
     default:
       return state
   }
