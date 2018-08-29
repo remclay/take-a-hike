99b901db77c97bf2e76f4a08e852ed9a12e91e6d
remclay <remmy.lauren@gmail.com>
2018-08-28 21:56:33 -0600

    Comment out DELETE_HIKES as not used - following removal of hike from API redirect to '/hikes', hikes fetched from API on componentDidMount so reducer action unnecessary.

--- client/src/reducers/hikes.js
+++ client/src/reducers/hikes.js
@@ -18,10 +18,10 @@ function hikesReducer(state = {loading: false, hikes: [], errors: []}, action) {
       const toUpdate = state.hikes.find(el => el.id === hikeDetails.id)
       const index = state.hikes.indexOf(toUpdate)
       return {loading: false, hikes: [...state.hikes.slice(0, index), hikeDetails, ...state.hikes.slice(index + 1)]}
-    case 'DELETE_HIKE':
-      // pass id to action
-      const newHikes = state.hikes.filter(el => el.id !== action.payload)
-      return {loading: false, errors: [], hikes: newHikes}
+    // case 'DELETE_HIKE':
+    //   // pass id to action
+    //   const newHikes = state.hikes.filter(el => el.id !== action.payload)
+    //   return {loading: false, errors: [], hikes: newHikes}
     case 'CLEAR_ERRORS':
       return {...state, errors: []}
     default:
