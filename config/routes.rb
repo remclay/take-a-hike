eaaf386844ea804a811d0c8459d29162b97d774a
remclay <remmy.lauren@gmail.com>
2018-09-06 19:31:34 -0600

    Add fallback route to api/routes, update node and npm versions, generally prepare app for heroku deploy.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :reviews
-  resources :hikes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :hikes
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
